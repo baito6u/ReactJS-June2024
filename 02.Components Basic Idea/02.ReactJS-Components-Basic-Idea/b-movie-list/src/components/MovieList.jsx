ec5babf224ba8a694888d1835141da91d9508ee5
Todor Krumov <baito6u@gmail.com>
2024-06-30 15:27:53 +0300

    Update MovieList.jsx

--- 02.Components Basic Idea/02.ReactJS-Components-Basic-Idea/b-movie-list/src/components/MovieList.jsx
+++ 02.Components Basic Idea/02.ReactJS-Components-Basic-Idea/b-movie-list/src/components/MovieList.jsx
@@ -1,20 +1,24 @@
 import MovieListItem from "./MovieListItem";
 
 export default function MovieList(props) {
-    return (
-        <>
-            <h1>{props.title}</h1>
+  return (
+    <>
+      <h1>{props.title}</h1>
 
-            <ul>
-                <MovieListItem url="https://www.imdb.com/title/tt0133093/">
-                    {props.movies[0]}
-                </MovieListItem>
-                <MovieListItem url="https://www.imdb.com/title/tt0770828/?ref_=fn_al_tt_1">{props.movies[1]}</MovieListItem>
-                <MovieListItem>{props.movies[2]}</MovieListItem>
-                <MovieListItem url="https://www.imdb.com/title/tt6113488/">
-                    {props.movies[3]}
-                </MovieListItem>
-            </ul>
-        </>
-    );
+      <ul>
+        <MovieListItem url="https://www.imdb.com/title/tt0133093/">
+          {props.movies[0]}
+        </MovieListItem>
+        <MovieListItem url="https://www.imdb.com/title/tt0770828/?ref_=fn_al_tt_1">
+          {props.movies[1]}
+        </MovieListItem>
+        <MovieListItem url="https://www.imdb.com/video/vi2933311001/?playlistId=tt7631058&ref_=tt_pr_ov_vi">
+          {props.movies[2]}
+        </MovieListItem>
+        <MovieListItem url="https://www.imdb.com/title/tt6113488/">
+          {props.movies[3]}
+        </MovieListItem>
+      </ul>
+    </>
+  );
 }

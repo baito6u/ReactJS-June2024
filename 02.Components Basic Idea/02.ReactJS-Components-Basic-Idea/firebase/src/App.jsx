2bd6703583480134a88d96884bd425a7b33a18d7
Todor Krumov <baito6u@gmail.com>
2024-07-02 06:26:26 +0300

    Update App.jsx

--- 02.Components Basic Idea/02.ReactJS-Components-Basic-Idea/firebase/src/App.jsx
+++ 02.Components Basic Idea/02.ReactJS-Components-Basic-Idea/firebase/src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import { Auth } from "./pages/auth";
+import { Tracker } from "./pages/traker";
 
 function App() {
-  return <></>;
+  return (
+  <div className="App">
+  <Router>
+    <Routes>
+      <Route path="/" exact element={<Auth/>}/>
+      <Route path="tracker" element={<Tracker/>}/>
+    </Routes>
+  </Router>
+  </div>
+  );
 }
 
 export default App;

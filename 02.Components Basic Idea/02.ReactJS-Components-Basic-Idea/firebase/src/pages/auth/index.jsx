be416004bddf6c697b72e91da15f8d5b3c3d8612
Todor Krumov <baito6u@gmail.com>
2024-07-02 06:27:22 +0300

    create auth/index.js

---
+++ 02.Components Basic Idea/02.ReactJS-Components-Basic-Idea/firebase/src/pages/auth/index.jsx
@@ -0,0 +1,28 @@
+import { auth, provider } from "../../config/firebase";
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
+export const Auth = () => {
+  const navigate = useNavigate();
+
+  const signInWithGoogle = async () => {
+    const result = await signInWithPopup(auth, provider);
+    const authInfo = {
+      userId: result.user.uid,
+      name: result.user.displayName,
+      profilePhoto: result.user.photoURL,
+      isAuth: true,
+    };
+    localStorage.setItem("auth", JSON.stringify(authInfo));
+    navigate("/tracker")
+  };
+
+  return (
+    <div className="login-page">
+      <p>Sign In With Google</p>
+      <button className="login-with-google-btn" onClick={signInWithGoogle}>
+        Sign In
+      </button>
+    </div>
+  );
+};

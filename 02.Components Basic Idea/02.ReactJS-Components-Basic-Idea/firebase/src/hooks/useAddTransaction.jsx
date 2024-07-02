329c02d726734e4b438bfbd32d76535f690412c0
Todor Krumov <baito6u@gmail.com>
2024-07-02 12:39:22 +0300

    Update useAddTransaction.jsx

--- 02.Components Basic Idea/02.ReactJS-Components-Basic-Idea/firebase/src/hooks/useAddTransaction.jsx
+++ 02.Components Basic Idea/02.ReactJS-Components-Basic-Idea/firebase/src/hooks/useAddTransaction.jsx
@@ -1,17 +1,24 @@
-import {addDoc, collection, serverTimestamp} from "firebase/firestore"
-import {db} from "../config/firebase"
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../config/firebase";
+
+import { useGetUserInfo } from "./useGetUserInfo";
 
 export const useAddTransaction = () => {
-    const transactionCollectionRef = collection(db, "transactions")
-    const addTransaction = async ({description, trasactionAmount, trasactionType}) => {
-        await addDoc(transactionCollectionRef, {
-            userId: "",
-            description,
-            trasactionAmount,    
-            trasactionType,
-            createdAt: serverTimestamp(),  
-        });
-    };
-    
-    return {addTransaction};
-}
+  const transactionCollectionRef = collection(db, "transactions");
+  const { userID } = useGetUserInfo();
+  const addTransaction = async ({
+    description,
+    trasactionAmount,
+    trasactionType,
+  }) => {
+    await addDoc(transactionCollectionRef, {
+      userID,
+      description,
+      trasactionAmount,
+      trasactionType,
+      createdAt: serverTimestamp(),
+    });
+  };
+
+  return { addTransaction };
+};

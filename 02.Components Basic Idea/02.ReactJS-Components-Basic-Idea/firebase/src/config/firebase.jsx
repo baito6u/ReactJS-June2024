fc8b3d12b429702aba2d8cdc62398d3065679cf3
Todor Krumov <baito6u@gmail.com>
2024-07-02 06:51:20 +0300

    change to jsx

--- 02.Components Basic Idea/02.ReactJS-Components-Basic-Idea/firebase/src/config/firebase.jsx
+++ 02.Components Basic Idea/02.ReactJS-Components-Basic-Idea/firebase/src/config/firebase.jsx
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from "firebase/auth"
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,10 +12,11 @@ const firebaseConfig = {
   projectId: "expense-tracker-db567",
   storageBucket: "expense-tracker-db567.appspot.com",
   messagingSenderId: "1073157256211",
-  appId: "1:1073157256211:web:73ece47e8402f7aead3be6"
+  appId: "1:1073157256211:web:73ece47e8402f7aead3be6",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider()
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
